247d546ee342478e042084e8b432ab9aef35b113
shinjiyun <ranji12@naver.com>
2024-07-02 17:46:14 +0900

    get방식의 fetch를 만들 것 이다.

---
+++ getFetch.js
@@ -0,0 +1,16 @@
+import http from 'http'
+const getUrl = {
+  url : (filePath)=>{
+    if(filePath){
+      if('/'){
+        
+      }
+    } 
+  }
+}
+
+const server = http.createServer((req,res)=>{
+  if(req.method==='GET'){
+    
+  }
+})
