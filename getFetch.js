36ee819d31d04426b04e4172875a301af3a2964f
jiyun <ranji12@naver.com>
2024-07-02 23:13:12 +0900

    getMethod 서버 작성

--- getFetch.js
+++ getFetch.js
@@ -1,16 +1,45 @@
 import http from 'http'
+import fs from 'fs'
+let port = 8080;
 const getUrl = {
-  url : (filePath)=>{
-    if(filePath){
-      if('/'){
-        
-      }
-    } 
+  filePath : (url)=>{
+    let filePath;
+    if(url === "/"){
+        filePath = "./public/getFetch.html";
+    }
+    else{
+      filePath = "./public" + url
+    }
+    return filePath;
   }
 }
 
 const server = http.createServer((req,res)=>{
+  let url = getUrl.filePath(req.url);
   if(req.method==='GET'){
-    
+    fs.readFile(url, (err,data)=>{
+      if(err){
+        if(err.code === "ENOENT"){
+          response.writeHead(404, {"Content-Type":"text/html"});
+          response.end("페이지를 찾을 수 없습니다.");
+        }
+        else{
+          response.writeHead(500);
+          response.end(`서버 오류: ${err.code}`);
+        }
+      }
+      else{
+        res.writeHead(200,{'Content-Tpye':'text/html; charset=utf-8'});
+        res.write(data);
+        res.end();
+      }
+    })
   }
+})
+
+server.listen(port,(err)=>{
+  if(err){
+    console.error(err)
+  }
+  console.log(`http://locahost:${port}`)
 })
